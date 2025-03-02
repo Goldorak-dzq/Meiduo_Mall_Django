cb553d4cfe16a35d7c3eb7bb37337e58f621ecaf
goldorak <1780315381@qq.com>
2025-03-02 15:18:55 +0800

    获取code->获取token->获取openid->保存openid

--- meiduo_mall/apps/oauth/urls.py
+++ meiduo_mall/apps/oauth/urls.py
@@ -2,9 +2,10 @@
 # @Author: DZQ 
 # @Time:2025/3/1 00:32
 from django.urls import path
-from apps.oauth.views import QQLoginURLView
+from apps.oauth.views import QQLoginURLView, OauthQQView
 
 urlpatterns = [
     path('qq/authorization/', QQLoginURLView.as_view()),
+    path('oauth_callback/', OauthQQView.as_view()),
 
 ]
--- meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/apps/oauth/views.py
@@ -54,9 +54,73 @@ class QQLoginURLView(View):
         # client_secret = None, appsecret
         # redirect_uri = None,  用户同意登录之后跳转的页面
         # state = None
+
         qq = OAuthQQ(client_id=settings.QQ_CLIENT_ID,
                      client_secret=settings.QQ_CLIENT_SECRET,
                      redirect_uri=settings.QQ_REDIRECT_URI,
                      state='xxx')
         qq_login_url = qq.get_qq_url()
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'login_url': qq_login_url})
+
+"""
+获取code->获取token->获取openid->保存openid
+
+前端
+   用户同意登录的code 把code发送给后端
+后端
+    请求:   获取code
+    业务逻辑:  获取code->获取token->获取openid->保存openid
+            根据openid进行判断
+            如果没有绑定，需要绑定
+            如果有绑定，直接登录                            
+    响应: 返回跳转链接
+    {'code':0, 'qq_login_url': "http://XXX"}
+    路由 GET  /oauth_callback/?code=xxx
+    
+    步骤: 
+        1.获取code
+        2.通过code获取token
+        3.通过token获取openid
+        4.根据openid判断 
+        5.如果没有绑定，需要绑定
+        6.如果有绑定，直接登录  
+       
+"""
+
+from apps.oauth.models import OAuthQQUser
+from django.contrib.auth import login
+class OauthQQView(View):
+    def get(self, request):
+        # 1.获取code
+        code = request.GET.get('code')
+        if code is None:
+            return JsonResponse({'code': 400, 'errmsg': '参数不全'})
+        # 2.通过code获取token
+        qq = OAuthQQ(client_id=settings.QQ_CLIENT_ID,
+                     client_secret=settings.QQ_CLIENT_SECRET,
+                     redirect_uri=settings.QQ_REDIRECT_URI,
+                     state='xxx')
+        # '76350B4707C29DF72B052C49A033A030'
+        token = qq.get_access_token(code)
+        # 3.通过token获取openid
+        # 'A56E1B92FD1BDE4E6B9B03EA842E0DE7'
+        openid = qq.get_open_id(token)
+        # 4.根据openid判断
+        try:
+            qquser = OAuthQQUser.objects.get(openid=openid)
+        except OAuthQQUser.DoesNotExist:
+            # 不存在
+            # 5.如果没有绑定，需要绑定
+            response = JsonResponse({'code': 400, 'access_token': openid})
+            return response
+        else:
+            # 存在
+            # 6.如果有绑定，直接登录
+            # 状态保持
+            # 6.1 设置session
+            login(request, qquser.user)
+            # 6.2 设置cookie
+            response = JsonResponse({'code': 0, 'errmsg': 'ok'})
+            response.set_cookie('username', qquser.user.username)
+            return response
+
