f47bb995715fb77c3821f1715d3ebcbc4a7e13a7
goldorak <1780315381@qq.com>
2025-03-03 21:00:07 +0800

    市区县数据获取

--- meiduo_mall/apps/areas/urls.py
+++ meiduo_mall/apps/areas/urls.py
@@ -2,9 +2,10 @@
 # @Author: DZQ 
 # @Time:2025/3/3 20:23
 from django.urls import path
-from apps.areas.views import AreaView
+from apps.areas.views import AreaView, SubAreaView
 
 urlpatterns = [
     path('areas/', AreaView.as_view()),
+    path('areas/<id>/', SubAreaView.as_view()),
 
 ]
--- meiduo_mall/apps/areas/views.py
+++ meiduo_mall/apps/areas/views.py
@@ -35,3 +35,39 @@ class AreaView(View):
             })
         # 3.返回信息
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'province_list': provinces_list})
+
+
+"""
+获取市区县信息
+
+前端
+   当页面修改市县的时候，会发生的axios请求 来获取下一条信息
+后端
+    请求:   传递省份id、市区id
+    业务逻辑:  根据id查询信息 将查询结果转换为字典列表    
+    响应: 返回JSON
+
+    路由 GET  areas/id/
+
+    步骤: 
+        1.获取省份id、市区id 查询信息
+        2.将对象转换为字典数据
+        3.返回响应
+
+"""
+class SubAreaView(View):
+    def get(self, request, id):
+        # 1.获取省份id、市区id 查询信息
+        # Area.objects.filter(parent_id=id)
+        # Area.objects.filter(parent=id)
+        up_level = Area.objects.get(id=id)  # 市
+        down_level = up_level.subs.all()  # 区县
+        # 2.将对象转换为字典数据
+        data_list = []
+        for down_level in down_level:
+            data_list.append({
+                'id': down_level.id,
+                'name': down_level.name,
+            })
+        # 3.返回响应
+        return JsonResponse({'code': 0, 'errmsg': 'ok', 'sub_data': {'subs': data_list}})
