9eab4f91248924a353e0b6a179f9add18a9995e3
goldorak <1780315381@qq.com>
2025-04-06 21:16:04 +0800

    实现is_staff用户登录

--- meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/apps/meiduo_admin/urls.py
@@ -3,10 +3,10 @@
 # @Time:2025/4/5 21:53
 
 from django.urls import path
-from rest_framework_jwt.views import obtain_jwt_token
+# from rest_framework_jwt.views import obtain_jwt_token
+from apps.meiduo_admin.utils import meiduo_token
 
 urlpatterns = [
-    path('authorizations/', obtain_jwt_token),
-
+    path('authorizations/', meiduo_token),
 
 ]
