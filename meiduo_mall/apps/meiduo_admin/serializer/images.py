48c3420c030d1e92b111ce80c74863aebc39ff13
goldorak <1780315381@qq.com>
2025-04-07 22:25:08 +0800

    实现图片上传更新

--- meiduo_mall/apps/meiduo_admin/serializer/images.py
+++ meiduo_mall/apps/meiduo_admin/serializer/images.py
@@ -19,6 +19,25 @@ class SKUImageModelSerializer(serializers.ModelSerializer):
         timestamp = int(time.time())
         return f"{cdn_base}{obj.image.url}?ts={timestamp}"
 
+    def update(self, instance, validated_data):
+        image_data = validated_data.get("image").read()
+        # 创建FastDFS连接对象
+        from fdfs_client.client import Fdfs_client
+        client = Fdfs_client('apps/meiduo_mall/utils/fastdfs/client.conf')
+        # 上传图片到fastDFS
+        result = client.upload_by_buffer(image_data)
+        # 判断是否上传成功
+        if result['Status'] != 'Upload successed.':
+            raise serializers.ValidationError('上传失败，请稍后再试')
+
+        file_id = result.get('Remote file_id')
+        # 更新模型的image数据
+        instance.sku_id = validated_data.get("sku_id")
+        instance.image = file_id
+        instance.save()
+        # 返回响应
+        return instance
+
 class ImageSKUModelSerializer(serializers.ModelSerializer):
 
     class Meta:
