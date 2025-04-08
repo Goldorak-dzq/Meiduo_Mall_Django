fb708c1504fb8869f7c3b97d9951d3ff964b9061
goldorak <1780315381@qq.com>
2025-04-08 21:02:01 +0800

    实现更新和删除SKU表数据

--- meiduo_mall/apps/meiduo_admin/serializer/sku.py
+++ meiduo_mall/apps/meiduo_admin/serializer/sku.py
@@ -2,13 +2,41 @@
 # @Author: DZQ 
 # @Time:2025/4/8 16:15
 from rest_framework import serializers
-from apps.goods.models import SKU, SPU, GoodsCategory, SPUSpecification, SpecificationOption
+from apps.goods.models import SKU, SPU, GoodsCategory, SPUSpecification, SpecificationOption, SKUSpecification
+# SKUSpecification序列化器 (SKU规格和规格选项)
+class SKUSpecificationModelSerializer(serializers.ModelSerializer):
+    spec_id = serializers.IntegerField()
+    option_id = serializers.IntegerField()
+    class Meta:
+        model = SKUSpecification
+        fields = ['spec_id', 'option_id']
 
 
+# SKU序列化器
 class SKUModelSerializer(serializers.ModelSerializer):
+    spu_id = serializers.IntegerField()
+    category_id = serializers.IntegerField()
+
+    # StringRelatedField本质获取关联模型的_str_的数据
+    spu = serializers.StringRelatedField(read_only=True)
+    category = serializers.StringRelatedField(read_only=True)
+    specs = SKUSpecificationModelSerializer(many=True)
     class Meta:
         model = SKU
         fields = '__all__'
+
+    def create(self, validated_data):
+        # 获取规格信息,并从validated_data数据中,删除规格信息数据
+        specs_data = validated_data.pop('specs')
+        # 保存sku
+        sku = SKU.objects.create(**validated_data)
+        # 对规格信息进行遍历,来保存商品规格信息
+        for spec_data in specs_data:
+            SKUSpecification.objects.create(sku=sku, **spec_data)
+        # 返回sku
+        return sku
+
+
 # 三级分类数据数据序列化器
 class GoodsCategoryModelSerializer(serializers.ModelSerializer):
     class Meta:
